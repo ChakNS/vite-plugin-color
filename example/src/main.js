0d452ea4e5077899c406b99d20885cc45c28c8d5
chak <chakcheung4565@outlook.com>
2022-03-16 21:55:45 +0800

    [update] example demo style

--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,10 @@
 import App from './app.vue'
 import { createApp } from 'vue'
-import './style/index.scss'
+import './style/demo.css'
+import './style/demo.scss'
+import './style/demo.less'
+import './style/demo.styl'
+import './style/element-ui.css'
+import './style/element-plus.css'
 
 createApp(App).mount('#app')
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,21 +2,41 @@ import vue from '@vitejs/plugin-vue'
 import viteColor from '../src'
 
 export default {
-	base: './',
-	plugins: [
-		vue(),
-		viteColor([
-			{
-				extract: ['#eee'],
-				output: 'themes/test-css_1.css',
-				injectTo: 'head'
-			},
-			{
-				extract: ['#ccc'],
-				output: 'themes/test-css_2.css',
-				transform: code => code.replace('#ccc', '#fff'),
-				injectTo: 'head'
-			}
-		])
-	]
+  base: './',
+  plugins: [
+    vue(),
+    viteColor([
+      {
+        extract: ['#eee'],
+        output: 'themes/triple-e.css',
+        injectTo: 'head',
+      },
+      {
+        extract: ['#409eff'],
+        output: 'themes/element-primary.css',
+      },
+      {
+        extract: ['#F56C6C'],
+        output: 'themes/element-danger-success.css',
+        transform: code => code.replace(new RegExp('#F56C6C', 'gi'), '#67C23A'),
+      },
+    ]),
+  ],
+	// remove @charset warning
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: atRule => {
+              if (atRule.name === 'charset') {
+                atRule.remove()
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
 }
